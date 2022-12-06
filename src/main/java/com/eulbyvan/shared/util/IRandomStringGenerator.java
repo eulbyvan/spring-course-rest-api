faa54a1a5691a4664f685f96a43c313109e3b645
eulbyvan <work.issdq@gmail.com>
2022-12-06 21:50:37 +0700

    removed uuid config

--- src/main/java/com/eulbyvan/shared/util/IRandomStringGenerator.java
+++ src/main/java/com/eulbyvan/shared/util/IRandomStringGenerator.java
@@ -1,12 +1,12 @@
-package com.eulbyvan.shared.util;
-
-import java.util.function.Supplier;
-
-/**
- * @author stu (https://www.eulbyvan.com/)
- * @version 1.0
- * @since 03/12/22
- */
-
-public interface IRandomStringGenerator extends Supplier<String> {
-}
+//package com.eulbyvan.shared.util;
+//
+//import java.util.function.Supplier;
+//
+///**
+// * @author stu (https://www.eulbyvan.com/)
+// * @version 1.0
+// * @since 03/12/22
+// */
+//
+//public interface IRandomStringGenerator extends Supplier<String> {
+//}
